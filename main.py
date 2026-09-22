dea58341f7163eab076fe4beb6b794d92a824284
Brielle Alexis christabel <email-brieliechristabelb@gmail.com>
2026-09-22 13:40:40 +0800

    Inisialisasi kode FastAPI dan requirements

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 """Module Name: REST API PIN Extraction Tool
    Description: Menghubungkan fungsi sanitasi PIN dengan framework FastAPI."""
 
-from fastapi import FastAPI, HTTPException
+import os
 from pydantic import BaseModel
-from app_v2_sanitized import process_text_to_pin
+import uvicorn
+from Fastapi import FastAPI
 # Inisialisasi Aplikasi FastAPI
-app = FastAPI(title="Secure PIN Generator API")
+app = FastAPI(title="PIN Extraction Tool", description="API For Extraction and PIN Sanitized", version="1.0.0")
 # Skema Input Data 
-class InputData(BaseModel):
+class Textinput(BaseModel):
     raw_text: str
-# Endpoint Utama (POST Request)
-@app.post("/generate_pin")
-def generate_pin(payload: InputData):
+# --- SEMUA ROUTE / ENDPOINT SANITASI PIN ---
+# --- Contoh: @app.post("/sanitized")
+if __name__ == "__main__":
+    # mengambil port otomatis dari environment render
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+def generate_pin(payload: Textinput):
     try: 
         pin_hasil = process_text_to_pin(payload.raw_text)
         return {"Status": "Sukses", "PIN": pin_hasil}
